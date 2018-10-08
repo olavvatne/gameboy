d92d997642b5321f3c111e05c5a2020db0cfdead
Olav Vatne <olavvatne@gmail.com>
2018-10-08 22:30:44 +0200

    Fix not all tests running travis

---
+++ test/memory/controller.test.js
@@ -0,0 +1,34 @@
+import { assert } from 'chai';
+import { it, beforeEach } from 'mocha';
+import { MMU } from '../../src/gameboy/memory';
+
+describe('Memory', () => {
+  let mmu = null;
+  beforeEach(() => {
+    mmu = new MMU();
+  });
+
+  describe('Controller tests', () => {
+    it('enables external ram if writing to an area', () => {
+      mmu.cartridge.type = 1;
+      assert.isFalse(mmu.cartridge.isExternalRam);
+      mmu.writeByte(0x1FFE, 0x0A);
+      assert.isTrue(mmu.cartridge.isExternalRam);
+    });
+
+    it('only enables ram if lower nibble is 0x0A', () => {
+      mmu.cartridge.type = 1;
+      assert.isFalse(mmu.cartridge.isExternalRam);
+      mmu.writeByte(0x1FFE, 0x01);
+      assert.isFalse(mmu.cartridge.isExternalRam);
+    });
+
+    it('disables external ram if any other value than 0x0A', () => {
+      mmu.cartridge.type = 1;
+      mmu.writeByte(0x1FFE, 0x0A);
+      assert.isTrue(mmu.cartridge.isExternalRam);
+      mmu.writeByte(0x1FFE, 0x0B);
+      assert.isFalse(mmu.cartridge.isExternalRam);
+    });
+  });
+});
