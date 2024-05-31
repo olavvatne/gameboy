7611cfb76fb9332b564e6e8049206f9a3231aaba
Olav Vatne <olavvatne@gmail.com>
2024-05-31 20:45:57 +0200

    feat: zoom in and out + center screen

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ if (!window.IS_PRODUCTION) {
 
 let gameboy = null;
 let data = null;
+let zoomFactor = 1.0;
 
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 const resetButton = document.querySelector('#reset');
+const zoomInButton = document.querySelector('#zoom-in');
+const zoomOutButton = document.querySelector('#zoom-out');
 
 function hide(element) {
   element.style.display = 'none';
@@ -42,10 +45,21 @@ const handleRomSelect = (evt) => {
 };
 
 window.onload = () => {
+  const root = document.documentElement;
+  root.style.setProperty('--zoom-factor', 1.0)
+  console.log(zoomFactor);
   initGameboy();
   startButton.onclick = () => {gameboy.start(data); hide(startButton); show(pauseButton)};
   pauseButton.onclick = () => {gameboy.pause(); hide(pauseButton); show(startButton)};
   resetButton.onclick = () => {gameboy.reset(); hide(pauseButton); show(startButton)};
+  zoomInButton.onclick = () => {
+    zoomFactor = Math.min(zoomFactor + 0.2, 3.0); 
+    root.style.setProperty('--zoom-factor', zoomFactor); 
+  };
+  zoomOutButton.onclick = () => {
+    zoomFactor = Math.max(zoomFactor - 0.2, 0.2); 
+    root.style.setProperty('--zoom-factor', zoomFactor); 
+  };
   document.addEventListener('keydown', e => gameboy.io.handleKeyDown(e));
   document.addEventListener('keyup', e => gameboy.io.handleKeyUp(e));
   document.getElementById('load').addEventListener('change', e => handleRomSelect(e), false);
